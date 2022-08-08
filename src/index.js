8f495f54134cad35c464713ab8aa4d417351007e
Angel Cubas <langelcubas9@gmail.com>
2022-08-07 21:55:45 -0500

    Clean root file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 import './js/index'; // importing js files
 import './scss/main.scss'// importing scss files
-
-// Create example heading node
-const heading = document.createElement('h1')
-heading.textContent = 'Interesting!'
-
-const app = document.querySelector('#root')
-app.append(heading)
